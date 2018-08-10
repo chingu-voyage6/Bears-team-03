7cf1dffb5cd00ec884712a36cc51f22b1ececfa5
jidemobell <jidd32@gmail.com>
2018-08-10 23:47:19 +0100

    add key to passport

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ let env = process.env.NODE_ENV || 'development';
 
 if(env === 'development'){
     process.env.PORT = 4000;
-    process.env.JWT_SECRET = cookies;
+    process.env.JWT_SECRET = 'cookies';
     process.env.MONGODB_URI = 'mongodb://localhost:27017/ExpenseApp'
 }else if(env == 'test'){
     process.env.PORT = 4000;
-    process.env.JWT_SECRET = cookies;
+    process.env.JWT_SECRET = 'cookies';
     process.env.MONGODB_URI = 'mongodb://localhost:27017/ExpenseAppTest'
 }else if(env == 'production'){
-  process.env.JWT_SECRET = cookies;
+  process.env.JWT_SECRET = 'cookies';
   process.env.MONGODB_URI = 'mongodb://<dbuser>:<dbpassword>@ds219432.mlab.com:19432/expenseapp'
 }
