137132c578c7c0373021a2b4f72e7e1d6286f33c
jidemobell <jidd32@gmail.com>
2019-08-23 16:52:05 +0100

    edit actions url

--- client/src/actions/User/UserActions.js
+++ client/src/actions/User/UserActions.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import * as actionType from '../actionTypes'
-import * as ur from '../../urls/URL';
 import history from '../../hoc/History/History'
 
-const url = returnUrls();
 
 export function signUp( {
   firstName,
