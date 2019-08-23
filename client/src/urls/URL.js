a9af06f75be35d41cbffd1d7b76a969c30babc9d
jidemobell <jidd32@gmail.com>
2019-08-23 15:09:30 +0100

    update url for build

--- client/src/urls/URL.js
+++ client/src/urls/URL.js
@@ -1,2 +1,2 @@
-export const USER_URL= 'https://chinguexpenseapp.herokuapp.com:4000/user'
-export const EXPENSE_URL =  'https://chinguexpenseapp.herokuapp.com:4000/expense'
+export const USER_URL= 'http://localhost:4000/user'
+export const EXPENSE_URL =  'http://localhost:4000/expense'
